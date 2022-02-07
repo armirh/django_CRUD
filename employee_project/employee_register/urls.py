c9c764141be6810cd627d50107f824d5f08929ea
Armir Halimi <armirhalimi15@gmail.com>
2022-02-07 07:02:33 -0600

    Second commit
    Fixing up frontend and adding functionality..

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.employee_form),  # localhost:p/employee/list
-    path('list/', views.employee_list),
+    # get and post req. operations
+    path('', views.employee_form, name='employee_insert'),
+    # get and post req. for update
+    path('<int:id>', views.employee_form, name='employee_update'),
+    # get req. to retrieve and show
+    path('list/', views.employee_list, name='employee_list'),
 ]
--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import EmployeeForm
+from .models import Employee
 # Create your views here.
 
 # function to retrieve and display employee record
 
 
 def employee_list(request):
-    return render(request, "employee_register/employee_list.html")
+    context = {'employee_list': Employee.objects.all()}
+    return render(request, "employee_register/employee_list.html", context)
 
 
-def employee_form(request):
-    return render(request, "employee_register/employee_form.html")
+def employee_form(request, id=0):
+    if request.method == 'GET':
+        if id == 0:
+            form = EmployeeForm()
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(instance=employee)
+        return render(request, "employee_register/employee_form.html", {'form': form})
+    else:
+        if id == 0:
+            form = EmployeeForm(request.POST)
+        else:
+            employee = Employee.objects.get(pk=id)
+            form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 
 
 def employee_delete(request):
